cad8923a2cf395daf4e6e5b3c8b13873950738fc
deenahrajeswari <deenahrajeswari@gmail.com>
2026-04-21 19:34:23 +0530

    Day1 sum of two numbers

---
+++ Day01_Sum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Day01_Sum{
+     public static void main(String args[]){
+      Scanner scan=new Scanner(System.in);
+      System.out.println("enter the first number");
+      int num1=scan.nextInt();
+      System.out.println("enter the second number");
+      int num2=scan.nextInt();
+
+      int sum=num1+num2;
+      scan.close();
+      System.out.println("the sum of two numbers is.." + sum);
+      
+     }
+}
